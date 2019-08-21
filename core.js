d055760dc460656d57181c07fa72b9ff1a9d8fce
Pat Mifsud <patmifsud@me.com>
2019-08-21 20:51:00 +1000

    Better logging
    reports the type of card added and it's attribures

--- core.js
+++ core.js
@@ -1,36 +1,72 @@
-
-
 // Cards
-var cBlock = {title:"Block", cost:20, def:10, rarity:0 }
-var cShield = {title:"Shield", cost:20, def:15, rarity:1}
-var cBig_Shield = {title:"Big Shield", cost:200, def:25, rarity:2 }
-var cStrike = {title:"Strike", cost:30, atk:5, rarity:0 }
-var cBolt = {title:"Bolt", cost:70, atk:10, rarity:1 }
-var cDefencive_Strike = {title:"Defencive_Strike", cost:200, def:20, atk:20, rarity:2 }
-var cBurn = {title:"Burn", cost:300, atk:20, effect:"burn", effectAttribute:3, rarity:2 }
+var cBlock = {
+    title: "Block",
+    cost: 20,
+    def: 10,
+    rarity: 0
+};
+var cShield = {
+    title: "Shield",
+    cost: 20,
+    def: 15,
+    rarity: 1
+};
+var cBig_Shield = {
+    title: "Big Shield",
+    cost: 200,
+    def: 25,
+    rarity: 2
+};
+var cStrike = {
+    title: "Strike",
+    cost: 30,
+    atk: 5,
+    rarity: 0
+};
+var cBolt = {
+    title: "Bolt",
+    cost: 70,
+    atk: 10,
+    rarity: 1
+};
+var cDefencive_Strike = {
+    title: "Defencive_Strike",
+    cost: 200,
+    def: 20,
+    atk: 20,
+    rarity: 2
+};
+var cBurn = {
+    title: "Burn",
+    cost: 300,
+    atk: 20,
+    effect: "burn",
+    effectAttribute: 3,
+    rarity: 2
+};
 
 // Script
 
 window.onload = function () {
 
-loadDivs();
+    loadDivs();
 
-dealButton.onclick = function(){
-    dealCard("deck", "hand");
-}
+    dealButton.onclick = function () {
+        dealCard("deck", "hand");
+    }
 
-shuffleButton.onclick = function(){
+    shuffleButton.onclick = function () {
+        shuffle(deck);
+    }
+
     shuffle(deck);
-}
-
-shuffle(deck);
 
 }
 
 // Setup
 
 
-var deck = [cBlock, cBlock, cBlock, cBlock, cBlock, cStrike, cStrike];
+var deck = [cBlock, cBolt, cBlock, cBlock, cBurn, cStrike, cStrike];
 var table = [];
 var hand = [];
 
@@ -39,34 +75,46 @@ var hand = [];
 
 // This funciton takes a card from a place (from) and puts it somewhere else (to). Use lowercase strings 
 function dealCard(from, to) {
-    var fromVar = deck;
-    var toVar = hand;
-    
-    if (from == "deck"){fromVar = deck; } 
-    else if (from == "table"){fromVar = table;}
-    else if (from == "hand"){fromVar = hand;};
+    var fromVar;
+    var toVar;
+    var toLimit;
 
-    if (to == "deck"){toVar = deck;} 
-    else if (to == "table"){toVar = table;}
-    else if (to == "hand"){toVar = hand;};
-    
-    if(fromVar.length > 0){
+    if (from == "deck") {
+        fromVar = deck;
+    } else if (from == "table") {
+        fromVar = table;
+    } else if (from == "hand") {
+        fromVar = hand;
+    } else {
+        console.log("Invalid params. deck table or hand as strings please.");
+        return;
+    };
+
+    if (to == "deck") {
+        toVar = deck;
+        toLimit = 50;
+    } else if (to == "table") {
+        toVar = table;
+        toLimit = 5;
+    } else if (to == "hand") {
+        toVar = hand;
+        toLimit = 6;
+    } else {
+        console.log("Invalid params. deck table or hand as strings please.");
+        return;
+    };
+
+    if (toVar.length > toLimit) {
+        console.log(to + " was full. No card drawn.");
+    } else if (fromVar.length > 0) {
         toVar.push(fromVar[0]);
         fromVar.shift();
-        console.log("Card drawn from " + from + " and placed on " + to );
+        console.log("Card drawn ", toVar.slice(-1)[0], " from ", from, " and placed on ", to);
         reportCardsToConsole();
     } else {
-        console.log("There are no cards in " + from + " to draw into " + to );
+        console.log("There are no cards in " + from + " to draw into " + to);
     }
-
-    // if (deck.length < 1){ alert("No cards in your deck"); console.log("Deck Empty"); } else{
-    //     // can only be 5 cards on the table. Get rid of zero if too many
-    //     if (table.length >= 5){ table.shift();  };
-    //     table.push(deck[0]);
-    //     deck.shift();
-    //     console.log("Card drawn from deck and placed on table");
-    // }
-}
+};
 
 function loadDivs() {
     var dealButton = document.getElementById("dealButton");
@@ -76,16 +124,21 @@ function loadDivs() {
 // misc
 // List all cards in hand, deck and table out in the console
 function reportCardsToConsole() {
-    var deckList = []; var handList = []; var tableList = [];
-    deck.forEach(function(obj){
-    deckList.push(obj.title);});
+    var deckList = [];
+    var handList = [];
+    var tableList = [];
+    deck.forEach(function (obj) {
+        deckList.push(obj.title);
+    });
 
-    hand.forEach(function(obj){
-    handList.push(obj.title);});
+    hand.forEach(function (obj) {
+        handList.push(obj.title);
+    });
 
-    table.forEach(function(obj){
-    tableList.push(obj.title);});
-    console.log("Deck (" + deck.length + " cards)" + deckList + " | " + "Hand (" + hand.length + " cards)" + handList + " | " + "Table ("  + table.length + " cards)" + tableList);
+    table.forEach(function (obj) {
+        tableList.push(obj.title);
+    });
+    console.log("Deck (" + deck.length + " cards)" + deckList + " | " + "Hand (" + hand.length + " cards)" + handList + " | " + "Table (" + table.length + " cards)" + tableList);
 }
 
 // from stackoverflow, don't know how it works https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
